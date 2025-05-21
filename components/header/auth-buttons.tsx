5268f18432198e73eaea46ca4b02a9eb9202dad0
apilarteq <alvaropilarte123@gmail.com>
2025-05-21 12:06:25 -0600

    Style: Change auth buttons colors and hide forgot password

--- components/header/auth-buttons.tsx
+++ components/header/auth-buttons.tsx
@@ -11,13 +11,13 @@ const HeaderAuthButtons = () => {
   return (
     <div className="flex items-center gap-x-4 text-black">
       <button
-        className="bg-gray-100 px-4 py-2 rounded-md hover:bg-gray-200 transition-all cursor-pointer"
+        className="bg-gray-100 px-4 py-2 rounded-md hover:bg-gray-300 active:bg-gray-200 transition-all cursor-pointer"
         onClick={() => handleModal("login")}
       >
         Sign in
       </button>
       <button
-        className="bg-gray-100 px-4 py-2 rounded-md hover:bg-gray-200 transition-all cursor-pointer"
+        className="bg-gray-100 px-4 py-2 rounded-md hover:bg-gray-300 active:bg-gray-200 transition-all cursor-pointer"
         onClick={() => handleModal("register")}
       >
         Sign up
